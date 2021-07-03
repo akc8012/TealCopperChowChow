e51344a0cfb00856e5ac5982c1189be3e3fb4ecc
akc8012 <akc8012@gmail.com>
2021-07-03 18:22:46 -0400

    Remove janky player stopping code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,9 +21,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
 	{
-		Vector3 lastDirection = Direction;
-		Vector3 lastPosition = transform.position;
-
 		if (Input.GetAxisRaw("Vertical") > 0)
 			Direction = new Vector3(0, 0, MoveSpeed);
 		if (Input.GetAxisRaw("Vertical") < 0)
@@ -34,12 +31,6 @@ public class PlayerController : MonoBehaviour
 			Direction = new Vector3(MoveSpeed, 0, 0);
 
 		CharacterController.Move(Direction * Time.deltaTime);
-
-		if (lastPosition == transform.position)
-		{
-			Direction = lastDirection;
-			CharacterController.Move(Direction * Time.deltaTime);
-		}
 	}
 
 	private void OnTriggerEnter(Collider other)
