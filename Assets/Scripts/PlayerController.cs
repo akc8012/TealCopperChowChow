e6052e23d73b83a54eed920d9db69c9a913c0c0b
akc8012 <akc8012@gmail.com>
2021-07-03 22:01:28 -0400

    Fix movement issues with 90000000 raycasts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,11 +6,17 @@ public class PlayerController : MonoBehaviour
 	private float MoveSpeed = 10;
 
 	private Vector3 Direction;
+	private const float RaycastDistance = 1f;
 
 	private CharacterController CharacterController;
-	
-    // Start is called before the first frame update
-    void Start()
+
+	private bool AllowedToMoveForward = true;
+	private bool AllowedToMoveBack = true;
+	private bool AllowedToMoveLeft = true;
+	private bool AllowedToMoveRight = true;
+
+	// Start is called before the first frame update
+	void Start()
 	{
 		CharacterController = GetComponent<CharacterController>();
 	}
@@ -21,16 +24,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
 	{
-		if (Input.GetAxisRaw("Vertical") > 0)
-			Direction = new Vector3(0, 0, MoveSpeed);
-		if (Input.GetAxisRaw("Vertical") < 0)
-			Direction = new Vector3(0, 0, -MoveSpeed);
-		if (Input.GetAxisRaw("Horizontal") < 0)
-			Direction = new Vector3(-MoveSpeed, 0, 0);
-		if (Input.GetAxisRaw("Horizontal") > 0)
-			Direction = new Vector3(MoveSpeed, 0, 0);
+		if (AllowedToMoveForward && Input.GetAxisRaw("Vertical") > 0)
+			Direction = Vector3.forward;
+		if (AllowedToMoveBack && Input.GetAxisRaw("Vertical") < 0)
+			Direction = Vector3.back;
+		if (AllowedToMoveLeft && Input.GetAxisRaw("Horizontal") < 0)
+			Direction = Vector3.left;
+		if (AllowedToMoveRight && Input.GetAxisRaw("Horizontal") > 0)
+			Direction = Vector3.right;
 
-		CharacterController.Move(Direction * Time.deltaTime);
+		CharacterController.Move(Direction * MoveSpeed * Time.deltaTime);
+	}
+
+	private void FixedUpdate()
+	{
+		var verticalPositions = new Vector3[] { transform.position, transform.position + new Vector3(0.5f, 0, 0), transform.position - new Vector3(0.5f, 0, 0) };
+		AllowedToMoveForward = AllowedToMoveInDirection(Vector3.forward, verticalPositions);
+		AllowedToMoveBack = AllowedToMoveInDirection(Vector3.back, verticalPositions);
+
+		var horizontalPositions = new Vector3[] { transform.position, transform.position + new Vector3(0, 0, 0.5f), transform.position - new Vector3(0, 0, 0.5f) };
+		AllowedToMoveLeft = AllowedToMoveInDirection(Vector3.left, horizontalPositions);
+		AllowedToMoveRight = AllowedToMoveInDirection(Vector3.right, horizontalPositions);
+	}
+
+	private bool AllowedToMoveInDirection(Vector3 direction, Vector3[] positions)
+	{
+		foreach (var position in positions)
+		{
+			var ray = new Ray(position, direction);
+			if (CollidingWithWall(ray))
+				return false;
+		}
+
+		return true;
+	}
+
+	private bool CollidingWithWall(Ray ray)
+	{
+		if (Physics.Raycast(ray, out RaycastHit hit, RaycastDistance))
+			return hit.collider.CompareTag("Wall");
+
+		return false;
 	}
 
 	private void OnTriggerEnter(Collider other)
