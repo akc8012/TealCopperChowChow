acd581931f6301fb8b64856c51470033214f23c8
Andrew Colannino <acolannino@emoneyadvisor.com>
2021-08-06 15:21:56 -0400

    Disappear life icons when die

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -1,13 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LifeManager : MonoBehaviour
 {
 	public int LifeCount = 3;
+	[SerializeField] private GameObject LifeManTop;
+	[SerializeField] private GameObject LifeManBottom;
 	
 	public void RemoveLife()
 	{
 		LifeCount--;
+
+		if (LifeCount == 2) 
+			LifeManBottom.SetActive(false);
+		
+		if (LifeCount == 1)
+			LifeManTop.SetActive(false);
+		
+		// todo: kill everything on 0
 	}
 }
