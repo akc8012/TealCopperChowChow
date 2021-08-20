9e8cb37e9dd938e022ebcaf32f21fb555b56115f
Andrew Colannino <acolannino@emoneyadvisor.com>
2021-08-20 16:03:41 -0400

    power pellet blinks

---
+++ Assets/Scripts/PowerPellet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPellet : MonoBehaviour
+{
+	private Renderer Renderer;
+    // Start is called before the first frame update
+    void Start()
+	{
+		Renderer = GetComponent<Renderer>();
+		StartCoroutine(nameof(BlinkRoutine));
+	}
+	
+	private IEnumerator BlinkRoutine()
+	{
+		while (true)
+		{
+			Renderer.enabled = !Renderer.enabled;
+			yield return new WaitForSeconds(.3f);
+		}
+	}
+}
